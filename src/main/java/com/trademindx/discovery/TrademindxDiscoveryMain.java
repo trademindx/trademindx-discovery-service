19f9a26ac1f2d8737b36888bf659b0948e781f7b
Alex Petlenko <alex@wallwest.co.uk>
2018-04-01 18:59:52 +0100

    minor changes

--- src/main/java/com/trademindx/discovery/TrademindxDiscoveryMain.java
+++ src/main/java/com/trademindx/discovery/TrademindxDiscoveryMain.java
@@ -1,4 +1,4 @@
-package com.trademindx.discovery.service;
+package com.trademindx.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServiceMain {
+public class TrademindxDiscoveryMain {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceMain.class, args);
+        SpringApplication.run(TrademindxDiscoveryMain.class, args);
     }
 }
